f9e1a9d780b607192fb1bb8008d2d07d4f26f824
LautaroSuarez <47385870+LautaroSuarez@users.noreply.github.com>
2020-02-12 11:03:44 -0300

    Add files via upload

--- jsInstruccionIF (10).js
+++ jsInstruccionIF (10).js
@@ -1,18 +1,22 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-/*  var n1 = 1;
-    var numeroAleatorio = Math.round(Math.random(n1)) * 10;                          ESTE TAMBIÉN FUNCIONA.
-    */
-    var numeroAleatorio = Math.floor(Math.random()) * 10 + 1;
-
+      var n1 = 1;
+    var numeroAleatorio = Math.round(Math.random(n1) * 10);                        /*  ESTE TAMBIÉN FUNCIONA. */ /*var num = Math.floor(Math.random()*10 + 1); */
+                                                                                   /* num = Math.floor(Math.random() * ((máximo+1) - minimo) + minimo);
+  /*  var numeroAleatorio = Math.floor(Math.random() * 10 + 1); */                  /* ejemplo: var num = Math.floor((Math.random() * (11-5))+5); */
+                                                                                        /* num es ahora un número entre 5 y 10, ambos incluidos */
     if (numeroAleatorio >= 9 && numeroAleatorio <= 10) {
 
-        alert("EXCELENTE");
+        alert("Tu nota es "+numeroAleatorio+" EXCELENTE");
 
     }
-    else if (numeroAleatorio >=) {
+    else if (numeroAleatorio >= 4 && numeroAleatorio <= 8) {
 
+        alert("Tu nota es "+numeroAleatorio+" APROBÓ");
+    }
+    else {
+        alert("Tu nota es "+numeroAleatorio+" VAMOS, LA PRÓXIMA SE PUEDE");
     }
 	
 
---
+++ jsInstruccionSwitch(5).js
@@ -0,0 +1,25 @@
+function mostrar()
+{
+//tomo la edad  
+    var laHora = document.getElementById('hora').value;
+  /*  var hora = laHora >= 7 && laHora >= 11 */
+
+//alert (laHora);
+    switch (true) {        /* ES IMPORTANTE EN EL PARENTESIS PONER SI ES VERDADERO O FALSO PARA LOS RANGOS.  */
+        case (laHora < 7): {       /* laHora >= 7 && laHora >= 11 TAMBIÉN SIRVE. */
+            alert("No");
+            break;
+        }
+        case (laHora < 12): {
+            alert("Es de mañana");
+            break;
+        }
+        default: {
+            alert("Te equivocaste.");
+            break;
+        }
+    }
+
+
+
+}//FIN DE LA FUNCIÓN

d12c57dbb1f16e7aa5edc88e192dbd5874b3acd1
LautaroSuarez <47385870+LautaroSuarez@users.noreply.github.com>
2020-02-20 12:19:25 -0300

    Add files via upload

---
+++ jsIteracionesFor (6).js
@@ -0,0 +1,23 @@
+function mostrar()
+{
+    var NumeroPar;
+    var NumerosEnPar;
+    var Numero = parseInt(prompt("Ingresar un número"));
+
+
+    for (Incremento = 2; Incremento <= Numero; Incremento+2) {
+
+        NumerosEnPar = Numero % 2;
+        
+        if (Numero%2 == 0) {
+
+            NumeroPar++;
+           
+        }
+
+        break;
+
+    }
+
+
+}//FIN DE LA FUNCIÓN

ef7e61e91ddddf40d70617cb3f6ba896366ff922
LautaroSuarez <47385870+LautaroSuarez@users.noreply.github.com>
2020-02-20 10:42:06 -0300

    Add files via upload

---
+++ jsIteracionesFor (5).js
@@ -0,0 +1,18 @@
+function mostrar()
+{
+
+    for (contador = 0; contador >= 0; contador++) {
+
+        var pregunta = prompt("Ingresar el número 9");
+
+        if (pregunta == "9") {
+
+            contador = contador - 99999999999999999999;
+
+        }
+
+
+    }
+
+
+}//FIN DE LA FUNCIÓN

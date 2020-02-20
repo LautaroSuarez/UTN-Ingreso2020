83e59494cb08f30726fa2b0881bc9fcdbe41f026
LautaroSuarez <47385870+LautaroSuarez@users.noreply.github.com>
2020-02-20 12:11:43 -0300

    Add files via upload

--- jsIteraciones (10).js
+++ jsIteraciones (10).js
@@ -27,20 +27,20 @@ function mostrar()
             cNegativos++;
         }
 
-        diferenciasPyN = positivos - negativos;
+        diferenciasPyN = positivos + negativos;
 
         if (num == 0) {
             ceros++;
         }
 
-        if (num % 2 == 0 & num != 0 || num % 2 == 0 && num != 0) {
+        if (num % 2 == 0) {
             cNumerosPares++;
         }
 
     }
     while (pregunta == respuesta)
 
-    document.write("1- La suma de los negativos es " + negativos + "<br> 2- La suma de los positivos es " + positivos + "<br> 3- La cantidad de números positivos es " + cPositivos + "<br> 4- La cantidad de números negativos es " + cNegativos + "<br> 5- La cantidad de 0 es " + ceros + "<br> 6- La cantidad de números pares es " + cNumerosPares + "<br> 7- El promedio de los positivos es " + Math.round(positivos / cPositivos) + "<br> 8- El promedio de los negativos es " + Math.round(negativos / cNegativos) + "<br> 9- La diferencia entre positivos y negativos es " + diferenciasPyN);
+    document.write("1- La suma de los negativos es " + negativos + "<br> 2- La suma de los positivos es " + positivos + "<br> 3- La cantidad de números positivos es " + cPositivos + "<br> 4- La cantidad de números negativos es " + cNegativos + "<br> 5- La cantidad de 0 es " + ceros + "<br> 6- La cantidad de números pares es " + cNumerosPares + "<br> 7- El promedio de los positivos es "+ (positivos / cPositivos) + "<br> 8- El promedio de los negativos es " + (negativos / cNegativos) + "<br> 9- La diferencia entre positivos y negativos es " + diferenciasPyN);
 
 
 }//FIN DE LA FUNCIÓN

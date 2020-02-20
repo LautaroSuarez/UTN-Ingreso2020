890aa615fed4327b15f247c02c20e209711a672d
LautaroSuarez <47385870+LautaroSuarez@users.noreply.github.com>
2020-02-20 10:34:36 -0300

    Add files via upload

---
+++ jsIteracionesFor (4).js
@@ -0,0 +1,16 @@
+function mostrar()
+{
+
+    for (contador = 0; contador >= 0; contador++) {
+
+        var pregunta = prompt("Ingresar la palabra BREAK");
+
+        if (pregunta == "BREAK") {
+
+            contador = contador - 9999999999999999999999999;
+        }
+
+    }
+
+
+}//FIN DE LA FUNCIÓN
